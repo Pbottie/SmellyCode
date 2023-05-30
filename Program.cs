b1394aca54c40d92efccc64a2b23cee1376e5edc
Kaami <pbottie@gmail.com>
2023-05-30 22:56:03 +0200

    Had superfluous code

--- Program.cs
+++ Program.cs
@@ -17,42 +17,7 @@ namespace CalculatorRPN
             calculator.Run();
 
         }
-
-        static void PrintStack(Stack<double> stack)
-        {
-            string output = "[";
-            foreach (var item in stack)
-            {
-                output += item.ToString() + ",";
-            }
-            output = output.Substring(0, output.Length - 1);
-            output += "]";
-            Console.WriteLine(output);
-
-        }
-
-
-        static void Calculate(Stack<double> stack, char command)
-        {
-            if (command == '+')
-            {
-                stack.Push(stack.Pop() + stack.Pop());
-            }
-            else if (command == '*')
-            {
-                stack.Push(stack.Pop() * stack.Pop());
-            }
-            else if (command == '-')
-            {
-                double d = stack.Pop();
-                stack.Push(stack.Pop() - d);
-            }
-            else if (command == '/')
-            {
-                double d = stack.Pop();
-                stack.Push(stack.Pop() / d);
-            }
-        }
     }
+}
         
 }
